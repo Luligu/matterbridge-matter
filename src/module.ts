cf1e2c8315ca773902d79e53e6afe2133508814d
Luligu <target.epithet-0h@icloud.com>
2026-06-20 14:50:10 +0100

    Bump package to `automator` v.3.1.12

--- src/module.ts
+++ src/module.ts
@@ -1,5 +1,5 @@
 import { MatterbridgeDynamicPlatform, type PlatformConfig, type PlatformMatterbridge } from 'matterbridge';
-import { type AnsiLogger } from 'matterbridge/logger';
+import type { AnsiLogger } from 'matterbridge/logger';
 
 /**
  * This is the standard interface for Matterbridge plugins.
